8100bf60f3e2c1629cfaef5309fcc80788b84fbe
Serhii Buniak <serhii.buniak@okta.com>
2021-01-14 19:24:15 +0200

    Verify Access Token. OKTA-354773

--- tests/integration/test_jwt_verifier.py
+++ tests/integration/test_jwt_verifier.py
@@ -17,7 +17,7 @@ async def test_verify_token():
     client_id = os.environ.get('CLIENT_ID')
     token = os.environ.get('OKTA_JWT')
     jwt_verifier = JWTVerifier(issuer, client_id)
-    assert await jwt_verifier.verify_token(token)
+    await jwt_verifier.verify_access_token(token)
 
 
 @pytest.mark.skipif(not is_env_set(),
