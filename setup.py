13fc58a0d3461ae872d995d96069450b9b48fef1
Serhii Buniak <serhii.buniak@okta.com>
2021-05-07 17:22:41 +0300

    Fix getting version from __init__.py

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ import os
 
 
 def get_version():
-    # Get version number from VERSION file
-    with open(os.path.join(os.path.dirname(__file__), "okta_jwt_verifier", "__init__.py")) \
-            as version_file:
-        # File has format: __version__ = '{version_number}'
-        line = version_file.read().split("=")
-        version_number = line[1].strip().replace("'", "")
-        return version_number
+    """Read version from package init file (__init__.py)"""
+    full_path = os.path.join((os.path.dirname((__file__))), 'okta_jwt_verifier', '__init__.py')
+    with open(full_path) as f:
+        for line in f:
+            if '__version__' in line:
+                return line.split('=')[1].strip().strip("'")
 
 
 setup(
