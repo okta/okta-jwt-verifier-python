360e016a756cc2080ed279c4ad8f550fd2b97e93
extreme4all <59823072+yentl-creyf@users.noreply.github.com>
2022-07-14 15:49:16 +0200

    sorted imports, formatted code with black

--- okta_jwt_verifier/error_messages.py
+++ okta_jwt_verifier/error_messages.py
@@ -1,6 +1,6 @@
 from .constants import FINDING_OKTA_APP_CRED, FINDING_OKTA_DOMAIN
 
-ERROR_MESSAGE_ORG_URL_WRONG_TYPE = ("Your Okta URL should be type of str.")
+ERROR_MESSAGE_ORG_URL_WRONG_TYPE = "Your Okta URL should be type of str."
 
 ERROR_MESSAGE_ORG_URL_MISSING = (
     "Your Okta URL is missing. You can copy "
@@ -8,9 +8,7 @@ ERROR_MESSAGE_ORG_URL_MISSING = (
     "Console. Follow these instructions to"
     f" find it: {FINDING_OKTA_DOMAIN}"
 )
-ERROR_MESSAGE_ORG_URL_NOT_HTTPS = (
-    "Your Okta URL must start with 'https'."
-)
+ERROR_MESSAGE_ORG_URL_NOT_HTTPS = "Your Okta URL must start with 'https'."
 ERROR_MESSAGE_AUTH_MODE_INVALID = (
     "The AuthorizationMode configuration "
     "option must be one of: "
@@ -24,15 +22,11 @@ ERROR_MESSAGE_ORG_URL_YOUROKTADOMAIN = (
     f"{FINDING_OKTA_DOMAIN}"
 )
 
-ERROR_MESSAGE_ORG_URL_ADMIN = (
-    "Your Okta domain should not contain -admin. "
-)
+ERROR_MESSAGE_ORG_URL_ADMIN = "Your Okta domain should not contain -admin. "
 
-ERROR_MESSAGE_ORG_URL_TYPO = (
-    "It looks like there's a typo in your Okta domain."
-)
+ERROR_MESSAGE_ORG_URL_TYPO = "It looks like there's a typo in your Okta domain."
 
-ERROR_MESSAGE_CLIENT_ID_WRONG_TYPE = ("Your client ID should be type of str.")
+ERROR_MESSAGE_CLIENT_ID_WRONG_TYPE = "Your client ID should be type of str."
 
 ERROR_MESSAGE_CLIENT_ID_MISSING = (
     "Your client ID is missing. You can copy it from the "
@@ -48,4 +42,4 @@ ERROR_MESSAGE_CLIENT_ID_DEFAULT = (
     f"instructions to find it: {FINDING_OKTA_APP_CRED}"
 )
 
-ERROR_MESSAGE_AUDIENCE_MISSING = ("Audience is missing.")
+ERROR_MESSAGE_AUDIENCE_MISSING = "Audience is missing."
